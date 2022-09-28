35383824c279e1b1846cd1a686d02539ca3bf35c
Marcelo <xibongu@hotmail.com>
2022-09-28 19:14:34 -0300

    teste

--- src/app/components/template/header/header.component.ts
+++ src/app/components/template/header/header.component.ts
@@ -1,19 +1,18 @@
 import { Component, OnInit } from '@angular/core';
 import {Observable, Observer} from 'rxjs';
 
-
-
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
-export class HeaderComponent implements OnInit {
+export class HeaderComponent {
  
-
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor() {
+   
   }
+   }
 
-}
+
+
+
