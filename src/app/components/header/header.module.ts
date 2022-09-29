a574c6d5f304b63fdc89de98f3ad712e88ea287b
Anderson Ferreira <andy.walter@hotmail.com>
2022-09-29 14:23:26 -0300

    Conclusão de template contato - falta redirecionamento de links para agendamento (whatsapp)

--- src/app/components/header/header.module.ts
+++ src/app/components/header/header.module.ts
@@ -1,12 +1,13 @@
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatDividerModule } from '@angular/material/divider';
+import { MatTabsModule } from '@angular/material/tabs';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header.component';
 
 @NgModule({
   declarations: [HeaderComponent],
-  imports: [CommonModule, MatDividerModule, MatToolbarModule],
+  imports: [CommonModule, MatDividerModule, MatToolbarModule, MatTabsModule],
   exports: [HeaderComponent],
 })
 export class HeaderModule {}
